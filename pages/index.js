81106eaf49c473600ba2af11530400429abb7b3f
Mayur2801 <100698001+Mayur2801@users.noreply.github.com>
2025-06-20 16:27:51 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>🚀 Hello from SSR-enabled Next.js</h1>
+      <p>This page is rendered on the server using Next.js</p>
+    </div>
+  );
+}
